fab6353917eedb84d3b3bbfc0246ec18848bdb9d
BertheAdrien <berthe.adrien@yahoo.fr>
2024-04-30 09:44:59 +0200

    first commit

---
+++ single.php
@@ -0,0 +1,70 @@
+<?php include('header.php'); ?>
+
+<!DOCTYPE HTML>
+	<div class="container">
+		<div id="fh5co-intro">
+			<div class="row animate-box">
+				<div class="col-md-8 col-md-offset-2 col-md-pull-2">
+					<h2>Architecture Building In Dublin, Ireland</h2>
+				</div>
+			</div>
+		</div>
+		<div id="fh5co-portfolio">
+			<div class="row">
+
+				<div class="col-md-4 col-md-push-8 sticky-parent">
+					<div class="detail" id="sticky_item">
+						<div class="animate-box">
+							<h2>Dublin Stadium</h2>
+							<span>Architectural Design</span>
+							<p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts far from the countries Vokalia and Consonantia, there live the blind texts.</p>
+							<p>Dignissimos asperiores vitae velit veniam totam fuga molestias accusamus alias autem provident. Odit ab aliquam dolor eius. </p>
+							<p><a class="btn btn-primary btn-demo" href="#"></i> View Project</a></p>
+						</div>
+					</div>
+				</div>
+
+				<div class="col-md-7 col-md-pull-4 image-content">
+					<div class="image-item  animate-box">
+						<img src="images/portfolio-5.jpg" class="img-responsive" alt="Free HTML5 Bootstrap Template by FreeHTML5.co">
+					</div>
+					<div class="image-item  animate-box">
+						<img src="images/portfolio-2.jpg" class="img-responsive" alt="Free HTML5 Bootstrap Template by FreeHTML5.co">
+					</div>
+					<div class="image-item  animate-box">
+						<img src="images/portfolio-4.jpg" class="img-responsive" alt="Free HTML5 Bootstrap Template by FreeHTML5.co">
+					</div>
+					<div class="image-item  animate-box">
+						<img src="images/portfolio-3.jpg" class="img-responsive" alt="Free HTML5 Bootstrap Template by FreeHTML5.co">
+					</div>
+					<div class="image-item  animate-box">
+						<img src="images/portfolio-7.jpg" class="img-responsive" alt="Free HTML5 Bootstrap Template by FreeHTML5.co">
+					</div>
+				</div>
+				
+			</div>
+		</div>
+	</div><!-- END container-wrap -->
+
+	<div class="gototop js-top">
+		<a href="#" class="js-gotop"><i class="icon-arrow-up2"></i></a>
+	</div>
+	<?php include('footer.php'); ?>
+	<!-- jQuery -->
+	<script src="js/jquery.min.js"></script>
+	<!-- jQuery Easing -->
+	<script src="js/jquery.easing.1.3.js"></script>
+	<!-- Bootstrap -->
+	<script src="js/bootstrap.min.js"></script>
+	<!-- Waypoints -->
+	<script src="js/jquery.waypoints.min.js"></script>
+	<!-- Flexslider -->
+	<script src="js/jquery.flexslider-min.js"></script>
+	<!-- Sticky Kit -->
+	<script src="js/sticky-kit.min.js"></script>
+	<!-- Main -->
+	<script src="js/main.js"></script>
+
+	</body>
+</html>
+
