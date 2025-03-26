b8e65677e59ece206a42326fc7db19379a3edb78
BertheAdrien <berthe.adrien@yahoo.fr>
2025-03-26 16:45:34 +0100

    marre

--- src/pages/mentionslegale.php
+++ src/pages/mentionslegale.php
@@ -1,5 +1,41 @@
 <?php
 $pageTitle = 'Mentionslegale - Adrien Berthe';
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+</body>
+
+Les présentes conditions générales fixent les modalités d’utilisation du service web portfolio.elianeperol.com.
+
+Éditeur et responsable de publication
+Le site est édité par Eliane Perol, responsable de publication.
+Contact : eliane.perol@gmail.com
+Ce site est un portfolio personnel non déclaré à la CNIL.
+
+Hébergement
+Ce site est hébergé par OVH.com Siège social : 2 rue Kellermann – 59100 Roubaix – France.
+
+Responsabilité
+Le contenu du site est fourni à titre indicatif et peut être modifié à tout moment. L'éditeur s'efforce de garantir l'exactitude des informations mais ne peut être tenu responsable des erreurs ou omissions. L'utilisateur est seul responsable de l'usage qu'il fait des informations disponibles sur le site.
+
+Propriété intelectuelle
+Tous les éléments du site (textes, images, logos, etc.) sont protégés par des droits de propriété intellectuelle. Toute reproduction ou utilisation sans autorisation préalable est interdite, sauf pour les courtes citations avec mention de la source.
+
+Responsabilité dans l’utilisation du service
+L'utilisateur s'engage à ne pas perturber le bon fonctionnement du site, à ne pas publier de contenus illicites et à respecter les lois en vigueur. L'éditeur se réserve le droit de supprimer tout commentaire ou contenu inapproprié.
+
+Traitement des données personnelles
+Le site ne collecte aucune donnée personnelle et n'utilise pas de cookies. Conformément à la loi "Informatique et Libertés", aucune donnée personnelle n'est enregistrée à l'insu de l'utilisateur. Le site n'est pas déclaré à la CNIL.
+
+</html>
 ?>
 
 
