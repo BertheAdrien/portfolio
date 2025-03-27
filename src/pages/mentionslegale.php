567fe6806ee129c38ce403e37076fcb32d8f641c
BertheAdrien <berthe.adrien@yahoo.fr>
2025-03-27 09:56:35 +0100

    mentions légales

--- src/pages/mentionslegale.php
+++ src/pages/mentionslegale.php
@@ -9,33 +9,37 @@ $pageTitle = 'Mentionslegale - Adrien Berthe';
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 </head>
-<body>
-    
+<h1>Mentions Légales</h1>
+    <p>Les présentes conditions générales fixent les modalités d’utilisation du service web <strong>portfolio.elianeperol.com</strong>.</p>
+
+    <h2>Éditeur et responsable de publication</h2>
+    <p>Le site est édité par <strong>Eliane Perol</strong>, responsable de publication.</p>
+    <p>Contact : <a href="mailto:berthe.adrien@yahoo.fr">berthe.adrien@yahoo.fr</a></p>
+    <p>Ce site est un portfolio personnel non déclaré à la CNIL.</p>
+
+    <h2>Hébergement</h2>
+    <p>Ce site est hébergé par <strong>OVH.com</strong></p>
+    <p>Siège social : 2 rue Kellermann – 59100 Roubaix – France.</p>
+
+    <h2>Responsabilité</h2>
+    <p>Le contenu du site est fourni à titre indicatif et peut être modifié à tout moment.</p>
+    <p>L'éditeur s’efforce de garantir l’exactitude des informations mais ne peut être tenu responsable des erreurs ou omissions.</p>
+    <p>L’utilisateur est seul responsable de l’usage qu’il fait des informations disponibles sur le site.</p>
+
+    <h2>Propriété intellectuelle</h2>
+    <p>Tous les éléments du site (textes, images, logos, etc.) sont protégés par des droits de propriété intellectuelle.</p>
+    <p>Toute reproduction ou utilisation sans autorisation préalable est interdite, sauf pour les courtes citations avec mention de la source.</p>
+
+    <h2>Responsabilité dans l’utilisation du service</h2>
+    <p>L’utilisateur s’engage à ne pas perturber le bon fonctionnement du site, à ne pas publier de contenus illicites et à respecter les lois en vigueur.</p>
+    <p>L'éditeur se réserve le droit de supprimer tout commentaire ou contenu inapproprié.</p>
+
+    <h2>Traitement des données personnelles</h2>
+    <p>Le site ne collecte aucune donnée personnelle et n’utilise pas de cookies.</p>
+    <p>Conformément à la loi "Informatique et Libertés", aucune donnée personnelle n’est enregistrée à l’insu de l’utilisateur.</p>
+    <p>Le site n’est pas déclaré à la CNIL.</p>
+
 </body>
-
-Les présentes conditions générales fixent les modalités d’utilisation du service web portfolio.elianeperol.com.
-
-Éditeur et responsable de publication
-Le site est édité par Eliane Perol, responsable de publication.
-Contact : eliane.perol@gmail.com
-Ce site est un portfolio personnel non déclaré à la CNIL.
-
-Hébergement
-Ce site est hébergé par OVH.com Siège social : 2 rue Kellermann – 59100 Roubaix – France.
-
-Responsabilité
-Le contenu du site est fourni à titre indicatif et peut être modifié à tout moment. L'éditeur s'efforce de garantir l'exactitude des informations mais ne peut être tenu responsable des erreurs ou omissions. L'utilisateur est seul responsable de l'usage qu'il fait des informations disponibles sur le site.
-
-Propriété intelectuelle
-Tous les éléments du site (textes, images, logos, etc.) sont protégés par des droits de propriété intellectuelle. Toute reproduction ou utilisation sans autorisation préalable est interdite, sauf pour les courtes citations avec mention de la source.
-
-Responsabilité dans l’utilisation du service
-L'utilisateur s'engage à ne pas perturber le bon fonctionnement du site, à ne pas publier de contenus illicites et à respecter les lois en vigueur. L'éditeur se réserve le droit de supprimer tout commentaire ou contenu inapproprié.
-
-Traitement des données personnelles
-Le site ne collecte aucune donnée personnelle et n'utilise pas de cookies. Conformément à la loi "Informatique et Libertés", aucune donnée personnelle n'est enregistrée à l'insu de l'utilisateur. Le site n'est pas déclaré à la CNIL.
-
 </html>
-?>
 
 
