d59498509ca3a4efb203b342888efe941e4390d3
BertheAdrien <berthe.adrien@yahoo.fr>
2025-03-28 09:05:18 +0100

    ajout page entreprise ticket

--- src/pages/realisations.php
+++ src/pages/realisations.php
@@ -71,6 +71,20 @@ $pageTitle = 'Realisations - Adrien Berthe';
                     </div>
                 </div>
             </div>
+            <div class="col-md-6 padding-right">
+                <div class="row">
+                    <div class="col-md-12 animate-box ">
+                    <h1>Ticketing</h1>
+                    <h4>Entreprise CGI</h4>
+                        <a href="?page=project-pro-ticketing" class="portfolio-grid border border-dark rounded">  
+                            <img src="public/assets/images/Ticketing.PNG" class="img-responsive" alt="Ticketing">
+                            <div class="desc">
+                                <h3>Systeme de ticketing en entreprise</h3>
+                            </div>
+                        </a>
+                    </div>
+                </div>
+            </div>
         </div>
     </div>
 </div><!-- END container -->
