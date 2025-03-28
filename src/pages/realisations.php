f796c0d0ea2c8745178b7575aeca939fc7b3f9cc
BertheAdrien <berthe.adrien@yahoo.fr>
2025-03-28 13:40:31 +0100

    ticket 2

--- src/pages/realisations.php
+++ src/pages/realisations.php
@@ -88,12 +88,12 @@ $pageTitle = 'Realisations - Adrien Berthe';
             <div class="col-md-6 padding-right">
                 <div class="row">
                     <div class="col-md-12 animate-box ">
-                    <h1>Ticketing</h1>
+                    <h1>Modification de programme</h1>
                     <h4>Entreprise</h4>
-                        <a href="?page=project-ticketing" class="portfolio-grid border border-dark rounded">  
-                            <img src="public/assets/images/Ticketing.PNG" class="img-responsive" alt="Ticketing">
+                        <a href="?page=project-modification-programme" class="portfolio-grid border border-dark rounded">  
+                            <img src="public/assets/images/ModificationProgramme.PNG" class="img-responsive" alt="Modification de programme">
                             <div class="desc">
-                                <h3>Systeme de ticketing en entreprise</h3>
+                                <h3>Systeme de modification de programme</h3>
                             </div>
                         </a>
                     </div>
