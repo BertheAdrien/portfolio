88af92326690308b31712e8f73d3c0f4de7b8c0e
BertheAdrien <berthe.adrien@yahoo.fr>
2025-04-09 10:54:44 +0200

    allez

--- src/pages/entreprise.php
+++ src/pages/entreprise.php
@@ -63,7 +63,7 @@ $pageTitle = 'Entreprise - Adrien Berthe';
 
 <style>
 .img-custom {
-  width: 300px; /* ou une taille fixe genre 600px */
+  width: 450px; /* ou une taille fixe genre 600px */
   max-width: 100%;
   height: auto;
   display: block;
