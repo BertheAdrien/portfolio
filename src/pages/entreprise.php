128ea65467cb1e0489cb838e6134cefba92ac8a0
BertheAdrien <berthe.adrien@yahoo.fr>
2025-04-09 10:43:33 +0200

    re

--- src/pages/entreprise.php
+++ src/pages/entreprise.php
@@ -51,8 +51,8 @@ $pageTitle = 'Entreprise - Adrien Berthe';
 				<div class="col-md-12 animate-box detail">
 				<p class="text-center">Lorsqu’une modification est apportée à un programme, CGI suit un processus rigoureux pour assurer le passage du développement à la mise en production, illustré par le schéma ci-dessous.
 				Par ailleurs, une branche dédiée aux interventions d’urgence est prévue pour les cas de force majeure.</p>
-            <ul class="text-center width-60 ">
-				<p><img src="/public/assets/images/Environnement.drawio.png" class="img-responsive" alt="Fonctionnement du cadre de gestion CGMG"></p>
+            <ul class="text-center">
+				<p><img src="/public/assets/images/Environnement.drawio.png" class="img-responsive max-width: 100%" alt="Fonctionnement du cadre de gestion CGMG"></p>
             </ul>
         </div>
 		</div>
