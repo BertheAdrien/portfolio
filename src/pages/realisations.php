3bf1a8e561c82dd993578c785f3c47cd89cffec7
BertheAdrien <berthe.adrien@yahoo.fr>
2025-04-23 17:15:05 +0200

    calculette imo

--- src/pages/realisations.php
+++ src/pages/realisations.php
@@ -47,17 +47,20 @@ $pageTitle = 'Realisations - Adrien Berthe';
 
             <!-- Deuxième ligne -->
 
+            <div class="row">
+
                 <div class="col-md-6 padding-right">
-                    <div class="col-md-12 animate-box">
-                        <h1>Calculette immobilière</h1>
-                        <h4>Ecole</h4>
-                        <a href="?page=calculette-imo" class="portfolio-grid border border-dark rounded">  
-                            <img src="public/assets/images/calculettePret.PNG" class="img-responsive" alt="Calculette immobilière">
-                            <div class="desc">
-                                <h3>Outil de gestion de taux de prêts immobiliers</h3>
-                            </div>
-                        </a>
-                    </div>
+                        <div class="col-md-12 animate-box">
+                            <h1>Portfolio</h1>
+                            <h4>Ecole</h4>
+                            <a href="?page=project-portfolio" class="portfolio-grid border border-dark rounded">  
+                                <img src="public/assets/images/Portfolio.PNG" class="img-responsive" alt="Projet Portfolio">
+                                <div class="desc">
+                                    <h3>Création de mon Portfolio</h3>
+                                </div>
+                            </a>
+                        </div>
+                    </div> 
                 </div>
 
                 <div class="col-md-6 padding-right">
@@ -73,27 +76,10 @@ $pageTitle = 'Realisations - Adrien Berthe';
                     </div>
                 </div>
             </div>
-
+            
             <div class="clearfix"></div>
 
-            <!-- Troisième ligne -->
-            <div class="row">
-                <div class="col-md-6 padding-right">
-                    <div class="col-md-12 animate-box">
-                        <h1>Portfolio</h1>
-                        <h4>Ecole</h4>
-                        <a href="?page=project-portfolio" class="portfolio-grid border border-dark rounded">  
-                            <img src="public/assets/images/Portfolio.PNG" class="img-responsive" alt="Projet Portfolio">
-                            <div class="desc">
-                                <h3>Création de mon Portfolio</h3>
-                            </div>
-                        </a>
-                    </div>
-                </div> 
-            </div>
-
-            <div class="clearfix"></div>
-
+            
         </div>
     </div>
 </div><!-- END container -->
